6895dc14878d010e28c179e4d03af4199798a051
ACLAIN <amaury.clain@viacesi.fr>
2024-03-26 09:15:30 +0100

    Jour 1 + debut jour 2

--- ConsoleApp1/Program2.cs
+++ ConsoleApp1/Program2.cs
@@ -6,24 +6,6 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class User
-    {
-        public int Id;
-        public string FirstName;
-        public string LastName;
-        public int Old;
-        public int Salary;
-        public int Tax;
-    }
     internal class Program2
-    {
-        User user1 = new User();
-
-        user1.Id = 1;
-        user1.FirstName = "John";
-        user1.LastName = "Doe";
-        user1.Old = 30;
-        user1.Salary = 50000;
-        user1.Tax = 20;
-    }
+    { }
 }
