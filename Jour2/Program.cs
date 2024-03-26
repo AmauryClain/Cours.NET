430d19c7258b362bb0b51f4b8d56ccdf0a663197
ACLAIN <amaury.clain@viacesi.fr>
2024-03-26 16:14:15 +0100

    fin etape 4

--- Jour2/Program.cs
+++ Jour2/Program.cs
@@ -4,7 +4,7 @@ public abstract class Animal
 {
     private string _name;
     public string Name { get { return _name; } protected set { _name = value; } }
-    
+
     public virtual void Move()
     {
         Console.WriteLine("Move");
@@ -15,6 +15,13 @@ public abstract class Animal
         Console.WriteLine("Sleep");
     }
 
+}
+
+public class Dog
+{
+    public string DogName { get; set; }
+    public string Rarity { get; set; }
+    public int DogId { get; set; }
 }
 
 public class Griffin : Animal
@@ -74,5 +81,27 @@ class Program
         dragon.Move2();
         dragon.Move();
         dragon.Sleep();
+
+        IList<Dog> dogsList = new List<Dog>()
+        {
+            new Dog() {DogId = 1, DogName = "Wolf", Rarity = "Rare"},
+            new Dog() {DogId = 2, DogName = "Snow", Rarity = "Common" },
+            new Dog() {DogId = 3, DogName = "Alexandre", Rarity = "Common"},
+            new Dog() {DogId = 4, DogName = "Timéo", Rarity = "Mythique"},
+            new Dog() {DogId = 5, DogName = "Amaury", Rarity = "Master"}
+        };
+
+        var testDogs = from dog in dogsList where dog.Rarity == "Common" orderby dog.DogName descending select dog;
+        var testDogs2 = dogsList.Where(dog => dog.Rarity == "Rare");
+        Console.WriteLine("Query Expression:");
+        foreach (Dog testDog in testDogs)
+        {
+            Console.WriteLine(testDog.DogName + " "+ testDog.DogId);
+        }
+        Console.WriteLine("Method Expression:");
+        foreach (Dog testDog2 in testDogs2)
+        {
+            Console.WriteLine(testDog2.DogName + " " + testDog2.DogId);
+        }
     }
 }
